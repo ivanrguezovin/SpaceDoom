ef7edaac6af8575d5a5a071b3a0a86a695c435d1
Iván <uo265368@uniovi.es>
2020-11-04 18:36:25 +0100

    Proyecto igual a solucion previa de plataformas

--- SpaceDoom/SpaceDoom/Game.cpp
+++ SpaceDoom/SpaceDoom/Game.cpp
@@ -1,5 +1,6 @@
-
 #include "Game.h"
+#include "GameLayer.h"
+#include "MenuLayer.h"
 
 Game::Game() {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
@@ -12,6 +13,14 @@ Game::Game() {
 	// Escalado de imágenes de calidad 
 	// https://wiki.libsdl.org/SDL_HINT_RENDER_SCALE_QUALITY
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+
+	menuLayer = new MenuLayer(this);
+	gameLayer = new GameLayer(this);
+	layer = menuLayer;
+
+	// fuentes
+	TTF_Init();
+	font = TTF_OpenFont("res/sans.ttf", 24);
 
 	loopActive = true; // bucle activo
 	loop();
@@ -26,8 +35,12 @@ void Game::loop() {
 		initTick = SDL_GetTicks();
 
 		// Controles
+		layer->processControls();
 		// Actualizar elementos
+		layer->update();
 		// Dibujar
+		layer->draw();
+
 
 		endTick = SDL_GetTicks();
 		differenceTick = endTick - initTick;
@@ -36,6 +49,48 @@ void Game::loop() {
 			SDL_Delay((1000 / 30) - differenceTick);
 		}
 	}
+}
+
+void Game::scale() {
+	scaledToMax = !scaledToMax;
+
+	if (scaledToMax) {
+		SDL_DisplayMode PCdisplay;
+		SDL_GetCurrentDisplayMode(0, &PCdisplay);
+		float scaleX = (float)PCdisplay.w / (float)WIDTH;
+		float scaleY = (float)PCdisplay.h / (float)HEIGHT;
+		// Necesitamos la menor de las 2 escalas para no deformar el juego
+		scaleLower = scaleX;
+		if (scaleY < scaleX) {
+			scaleLower = scaleY;
+		}
+		// Cambiar dimensiones ventana
+		SDL_SetWindowSize(window, WIDTH * scaleLower, HEIGHT * scaleLower);
+		// Cambiar escala del render
+		SDL_RenderSetScale(renderer, scaleLower, scaleLower);
+	}
+	else { // Escala Original
+		scaleLower = 1;
+		// Cambiar dimensiones ventana
+		SDL_SetWindowSize(window, WIDTH, HEIGHT);
+		// Cambiar escala del render
+		SDL_RenderSetScale(renderer, 1, 1);
+	}
+
+}
+
+SDL_Texture* Game::getTexture(string filename) {
+	if (mapTextures.find(filename) != mapTextures.end()) {
+		cout << "retorno recurso cacheado" << filename << endl;
+	}
+	else {
+		cout << "Nuevo hay que cachearlo " << filename << endl;
+		SDL_Surface* surface = IMG_Load(filename.c_str());
+		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+		mapTextures[filename] = texture;
+	}
+
+	return mapTextures[filename];
 }
 
 
